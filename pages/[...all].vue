18b41c16be9c17861d168b61c9f71a992c71af60
Vlad <vladpoienariu@gmail.com>
2025-06-13 20:09:21 +0300

    Bravo rarese imi place de tine  ❎

---
+++ pages/[...all].vue
@@ -0,0 +1,35 @@
+<script setup lang='ts'>
+
+</script>
+
+<style>
+.wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100dvh;
+
+}
+
+h1 {
+    font-size: 64px;
+}
+
+p {
+    font-size: 32px;
+}
+
+.accent {
+    color: red;
+}
+
+</style>
+
+<template>
+    <div class="wrapper">
+        <h1>Pune bai rarese un 404</h1>
+        <p>Buna ziua, Muntean Reresh from <span class="accent">Dapyx</span> if I'm right?</p>
+    </div>
+</template>
